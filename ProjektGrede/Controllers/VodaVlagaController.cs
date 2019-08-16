c3f6a88f6693dec015a5d856389825b174f255f5
Barbara Pusnar <barbara.pusnar@scng.si>
2019-08-16 17:32:42 +0200

    popravki

--- ProjektGrede/Controllers/VodaVlagaController.cs
+++ ProjektGrede/Controllers/VodaVlagaController.cs
@@ -20,17 +20,33 @@ namespace ProjektGrede.Controllers
         {
             int stevilka = id ?? 1;
 
-            var data = from x in db.PodatkiSenzorjev
-                        where x.IdGrede == stevilka
-                        orderby x.Id descending
-                        select x;
+            //var data = from x in db.PodatkiSenzorjev
+            //            where x.IdGrede == stevilka
+            //            orderby x.Id descending
+            //            select x;
             //podatki za eno uro prvih 15 minut od ure
-            var dataDan = (from x1 in db.VodaVlaga
-                          // where x1.IDGrede == stevilka                          
-                           orderby x1.Datum 
-                           select new OdvisnostiViewModel {IdGrede=(int)x1.IDGrede,koo=new Koordinate {x = (decimal)x1.Padavine, y = (decimal)x1.Vlaga } });
+            //var dataDan = (from x1 in db.VodaVlaga
+            //              // where x1.IDGrede == stevilka                          
+            //               orderby x1.Datum 
+            //               select new OdvisnostiViewModel {IdGrede=(int)x1.IDGrede,koo=new Koordinate {x = (decimal)x1.Padavine, y = (decimal)x1.Vlaga } });
+            var dataDan1 = (from x1 in db.VodaVlaga
+                                // where x1.IDGrede == stevilka                          
+                            orderby x1.Datum
+                            select new OdvisnostiBubble { IdGrede = (int)x1.IDGrede, koo = new KoordinateBubble { x = (DateTime)x1.Datum, y = (decimal)x1.Vlaga, r = (decimal)x1.Padavine } });
+            List<OdvisnostiBubble1> prirejeni = new List<OdvisnostiBubble1>();
+            foreach (var x1 in dataDan1)
+            {
+                OdvisnostiBubble1 nov = new OdvisnostiBubble1();
+                nov.IdGrede = x1.IdGrede;
+                nov.koo = new KoordinateBubble1();
+                nov.koo.y = x1.koo.y;
+                nov.koo.r = x1.koo.r * 2;
+                nov.koo.x = x1.koo.x.DayOfYear - DateTime.Parse("31.8.2018").DayOfYear;
+                prirejeni.Add(nov);
+            }
             ViewData["id"] = stevilka;
-            return View(dataDan);
+           // return View(dataDan);
+            return View(prirejeni);
         }
 
         // GET: VodaVlaga/Details/5
---
+++ ProjektGrede/Models/OdvisnostiBubble.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektGrede.Models
+{
+    public class KoordinateBubble
+    {
+        public DateTime x; //dan
+        public decimal y; //vlaga
+        public decimal r; //padavine+zalivanje -- pazi to je v pixlih
+    }
+    public class KoordinateBubble1
+    {
+        public decimal x; //dan
+        public decimal y; //vlaga
+        public decimal r; //padavine+zalivanje -- pazi to je v pixlih
+    }
+    public class OdvisnostiBubble
+    {
+        public int IdGrede { get; set; }
+        public KoordinateBubble koo { get; set; }
+    }
+    public class OdvisnostiBubble1
+    {
+        public int IdGrede { get; set; }
+        public KoordinateBubble1 koo { get; set; }
+    }
+}
